9a261f7565c12c6854b2d433c69cddd016fd5679
hong9Lee <hongtemp9mac@gmail.com>
2025-01-29 21:10:54 +0900

    init

---
+++ src/main/kotlin/com/task/pjtredis/PjtRedisApplication.kt
@@ -0,0 +1,11 @@
+package com.task.pjtredis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PjtRedisApplication
+
+fun main(args: Array<String>) {
+    runApplication<PjtRedisApplication>(*args)
+}
---
+++ src/test/kotlin/com/task/pjtredis/PjtRedisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.task.pjtredis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PjtRedisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
