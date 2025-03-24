ca1b72dd67d8e1fcb0a8580042391cb6e6b9d5aa
hong9Lee <hongtemp9mac@gmail.com>
2025-03-24 20:08:37 +0900

    chore: db 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,16 +19,27 @@ repositories {
     mavenCentral()
 }
 
+val flywayVersion by extra { "10.4.1" }
+val loggerVersion by extra { "5.1.0" }
+val redissonVersion by extra { "3.21.3" }
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    implementation ("org.springframework.boot:spring-boot-starter-web")
+
     /** database */
     implementation("redis.clients:jedis:4.3.1")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.redisson:redisson-spring-boot-starter:$redissonVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.33")
     runtimeOnly("com.h2database:h2")
+
+    /** flyway */
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-mysql:$flywayVersion")
 
     val queryDslVersion = dependencyManagement.importedProperties["querydsl.version"]
     implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
@@ -36,6 +47,9 @@ dependencies {
 
     kapt("jakarta.persistence:jakarta.persistence-api")
     kapt("jakarta.annotation:jakarta.annotation-api")
+
+    // logging
+    implementation("io.github.oshai:kotlin-logging-jvm:$loggerVersion")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
